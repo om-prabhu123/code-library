3f4321f956822f474362dc856e7c29330bf4ab1e
om-prabhu123 <95235307+om-prabhu123@users.noreply.github.com>
2023-03-14 19:34:23 +0530

    Create square root of integer.cpp

---
+++ Sorting/square root of integer.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int countSquares(int N) {
+        // code here
+        
+       float f=sqrt(N);
+       int n=ceil(f);
+       return n-1;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.countSquares(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
