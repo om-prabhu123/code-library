4b0895d8a83ce37614350ad6337236f8d4d00a05
om-prabhu123 <95235307+om-prabhu123@users.noreply.github.com>
2023-03-26 11:39:21 +0530

    Create Minimum Swaps to Sort.cpp

---
+++ Sorting/Minimum Swaps to Sort.cpp
@@ -0,0 +1,59 @@
+    //{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution 
+{
+    public:
+    //Function to find the minimum number of swaps required to sort the array. 
+	int minSwaps(vector<int>&nums)
+	{
+	    // Code here
+	    vector<pair<int,int>>p;
+	    for(int i=0;i<nums.size();i++)
+	    {
+	            p.push_back({nums[i],i});
+	    }
+	    
+	    sort(p.begin(),p.end());
+	    
+	    int swaps=0;
+	    
+	    for(int i=0;i<p.size();i++)
+	    {
+	        
+	        int index=p[i].second;
+	        
+	        if(i!=index)
+	        {
+	            //actually this is swaped with some number
+	            swaps++;
+	            swap(p[i],p[index]);
+	            i--;  //again check is it swaped with any one else
+	        }
+	    }
+	    return swaps;
+	    
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>nums(n);
+		for(int i = 0; i < n; i++)
+			cin >> nums[i];
+		Solution obj;
+		int ans = obj.minSwaps(nums);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
