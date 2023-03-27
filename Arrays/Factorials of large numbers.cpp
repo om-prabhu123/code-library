1e95bceb11c0d3eab7127abdfe49e2ec7f42d363
om-prabhu123 <95235307+om-prabhu123@users.noreply.github.com>
2023-03-27 22:52:54 +0530

    Create Factorials of large numbers.cpp

---
+++ Arrays/Factorials of large numbers.cpp
@@ -0,0 +1,23 @@
+#User function Template for python3
+import math
+class Solution:
+    def factorial(self, N):
+        #code here
+        r=math.factorial(N)
+        return str(r)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.factorial(N);
+        for i in ans:
+            print(i,end="")
+        print()
+    
+# } Driver Code Ends
